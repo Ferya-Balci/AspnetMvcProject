acd3781e2fb53da2cf6b19cb593bc08a086588ac
Ferya Balci <feryaball@gmail.com>
2021-12-28 22:51:13 +0300

    Aspnet Mvc Proje dosyaları yüklendi

---
+++ TravelTripProje/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+
+        //context sınıfında c isimli nesne türettip bu c nesnesi yardımıyla da contexte bağlı olan sınıflar içinde de hakkımızda tablosuna ulaşıp bu tabloyuda listeledim.
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum(); //blogyorum'dan nesne türettim
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList(); //3 tane blok alacak
+            return View(by);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList(); //Linq sorgu oluşturdum.bloğun id'si gönderdiğim parametreye'ye eşit olan bloğun listesini getirecek 
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+
+        [HttpGet]
+        public PartialViewResult YorumYap(int id) //id değeri tıklandığında otomatik gelsin diye id tanımlandı.
+        {
+            ViewBag.deger = id; //viewbag ile degeri atadık
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar y)
+        {
+            c.Yorumlars.Add(y);
+            c.SaveChanges();
+            return PartialView();
+        }
+    }
+}
